e517f68a6213a3573c1ad330035d6a6b36cfcb33
uraza116 <uraza973@gmail.com>
2025-11-17 14:06:44 -0500

    first commit to add template

---
+++ template.py
@@ -0,0 +1,50 @@
+import os
+from pathlib import Path
+import logging
+logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s:')
+project_name="cnnClassifier"
+
+list_of_files = [
+    ".github/workflow/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    # f"src/{src_repo}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    # f"src/{src_repo}/entity/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    # "data/text.csv",
+    "config/config.yaml",
+    "dvc.yaml"
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb"
+    # "schema.yaml",
+    # "main.py",
+    # "app.py",
+
+    
+    
+    # "templates/index.html"
+]
+
+for filepath in list_of_files:
+        filepath = Path(filepath)
+        filedir, filename = os.path.split(filepath)
+
+        if filedir != "":
+            os.makedirs(filedir, exist_ok=True)
+            logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+        if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+            with open(filepath, "w", encoding="utf-8") as f:
+                pass
+                logging.info(f"Createing empty file: {filepath}")
+        else:
+            logging.info(f"{filename} already exist")
+
+ 
